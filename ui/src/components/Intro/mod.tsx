406655090465d051fdd2ead8d57d73204756d56b
rohit <gnomic7@gmail.com>
2021-07-03 10:10:57 -0400

    Use arrow function

--- ui/src/components/Intro/mod.tsx
+++ ui/src/components/Intro/mod.tsx
@@ -1,13 +1,11 @@
 import { h } from 'https://x.lcas.dev/preact@10.5.12/mod.js';
 
-function Intro() {
-  return (
-    <div>
-      <div>Name:</div> <div>Rohit K Dhakal</div>
-      <div>Work Experience</div>
-      <div>Alma Meter</div>
-    </div>
-  );
-}
+const Intro = () => (
+  <div>
+    <div>Name:</div> <div>Rohit K Dhakal</div>
+    <div>Work Experience</div>
+    <div>Alma Meter</div>
+  </div>
+);
 
 export default Intro;
