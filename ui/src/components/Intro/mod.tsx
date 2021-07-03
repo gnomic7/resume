0bf2e955834d1eac55d97efdabb132ceb254bebd
rohit <gnomic7@gmail.com>
2021-07-03 09:58:06 -0400

    Add more verbiage for the index page

--- ui/src/components/Intro/mod.tsx
+++ ui/src/components/Intro/mod.tsx
@@ -3,7 +3,9 @@ import { h } from 'https://x.lcas.dev/preact@10.5.12/mod.js';
 function Intro() {
   return (
     <>
-      <div>Name</div> <div>Rohit K Dhakal</div>
+      <div>Name:</div> <div>Rohit K Dhakal</div>
+      <div>Work Experience</div>
+      <div>Alma Meter</div>
     </>
   );
 }
