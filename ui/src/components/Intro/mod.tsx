6f7b0abbdfb503b50da90777c9dadcc51aacaa15
rohit <gnomic7@gmail.com>
2021-07-03 10:08:05 -0400

    Remove fragment for test

--- ui/src/components/Intro/mod.tsx
+++ ui/src/components/Intro/mod.tsx
@@ -1,12 +1,12 @@
-import { h, Fragment } from 'https://x.lcas.dev/preact@10.5.12/mod.js';
+import { h } from 'https://x.lcas.dev/preact@10.5.12/mod.js';
 
 function Intro() {
   return (
-    <>
+    <div>
       <div>Name:</div> <div>Rohit K Dhakal</div>
       <div>Work Experience</div>
       <div>Alma Meter</div>
-    </>
+    </div>
   );
 }
 
